063a086a4c54b22b478e5f4252e2a4d13ffd0a65
WORK-qdc <qclose@tcco.com>
2025-07-07 11:22:57 -0400

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,9 @@
+// js/config.js
+export const msalConfig = {
+  auth: {
+    clientId: '8e23d112-104b-4e6a-a57d-7e3a2a61d837',
+    authority: 'https://login.microsoftonline.com/20e27700-b670-4553-a27c-d8e2583b3289',
+    redirectUri: 'https://work-qdc.github.io/Scheduling-Dashboard/'
+  },
+  cache: { cacheLocation: 'localStorage' }
+};
